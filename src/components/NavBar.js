c7f712ec41c34b703653571a420367cc68869622
Zack Njine Wachira <zackwachira45@gmail.com>
2024-08-10 14:59:06 +0300

    Initial commit

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,24 +1,24 @@
 import { NavLink } from "react-router-dom";
-import "./NavBar.css";
+import "./NavBar.css"
 
 function NavBar() {
   return (
     <nav>
       <NavLink
         to="/"
-        className="nav-link"
+        className= "nav-link"
       >
         Home
       </NavLink>
       <NavLink
         to="/about"
-        className="nav-link"
+        className= "nav-link"
       >
         About
       </NavLink>
       <NavLink
         to="/login"
-        className="nav-link"
+        className= "nav-link"
       >
         Login
       </NavLink>
